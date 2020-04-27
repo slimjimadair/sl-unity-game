2c7b8923c6fbdd2ca020e4aea051ddc15af02aed
slimjimadair <slimjimadair@gmail.com>
2020-04-27 15:13:25 +0100

    Episode 1
    Added Player Controller: movement and look at cursor.

---
+++ sl-unity-game/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour
+{
+    // Variables
+    public float moveSpeed;
+    
+    PlayerController controller;
+    Camera viewCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WASD (or thumbstick) movement
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        // Point towards cursor
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            controller.LookAt(point);
+        }
+    }
+}
---
+++ sl-unity-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    Vector3 velocity;
+    Rigidbody myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Move function
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    // LookAt function
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedLookPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedLookPoint);
+    }
+
+    // FixedUpdate calls independent of frame rate to avoid collision issues
+    public void FixedUpdate()
+    {
+        myRigidBody.MovePosition(myRigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
